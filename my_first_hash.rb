dfd5abf12d2cebc3928e1732804214c410f36657
William Rogge <williamroggeiv@gmail.com>
2020-05-15 00:06:10 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -27,7 +27,7 @@ def adding
                   "oil paintings" => 3
                  }
                  shipping_manifest["muskets"] = 2, 
-                 shipping_manifest["gun powder"] = 4
+                 shipping_manifest["gun powder"] = 4  
   # add 2 muskets to the shipping_manifest hash below
 
 
